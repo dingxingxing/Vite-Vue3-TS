c291cdb17057a0dafbc3c2f23c502944cad37108
dingxingxing <1693992774@qq.com>
2021-07-12 20:16:05 +0800

    Vite+Vue3+TS 干就完事了

---
+++ src/components/Demo/Basic.vue
@@ -0,0 +1,62 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import css from '../../assets/index.css'
+export default defineComponent ({
+  name: "HelloWorld",
+  setup() {
+    // 代码这里
+  }
+})
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
+  <h3 class="text">components/HelloWorld.vue</h3>
+  <h1>{{ num }}</h1>
+</template>
+
+<!-- <script setup lang="ts">
+import { defineProps, ref, reactive, onMounted  } from 'vue'
+const color = "red"
+const font = {
+  size: "3rem"
+}
+defineProps({
+  msg: String
+})
+ref: num = 100;
+
+const state = reactive({ count: 0 })
+
+onMounted: {
+  $num = 200;
+  console.log("挂载", $num);
+}
+</script>
+
+<style scoped>
+/* :root {
+  --varColor: yellowgreen;
+} */
+a {
+  color: var(--varColor);
+}
+.text {
+  color: v-bind(color);
+  font-size: v-bind("font.szie");
+}
+</style> -->
